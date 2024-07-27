00bdcf672fd790ba5b0b80a3bc47c5b7bf2210d4
Caique Morales <caiquemorales20@gmail.com>
2024-07-27 19:48:20 -0300

    feat: add eslint + shadcn-ui

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <main className="w-screen bg-red-500">
+      <Button>Send</Button>
+    </main>
+  )
+}
